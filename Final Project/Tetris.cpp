d84ddb8b1dc3371ef023650862af33be0aac52ad
NighthawkLabs <116287980+NighthawkLabs@users.noreply.github.com>
2024-10-31 14:43:55 -0400

    Update Tetris.cpp

--- Final Project/Tetris.cpp
+++ Final Project/Tetris.cpp
@@ -83,3 +83,103 @@ bool canMoveDown(char board[boardRows][boardCols], char** tetromino, int tetroRo
     }
     return true;
 }
+
+// Define the rotations map explicitly
+map<int, vector<char**>> rotations = {
+    {1, {convertToPointer(I_0), convertToPointer(I_90)}},
+    {3, {convertToPointer(T_0), convertToPointer(T_90), convertToPointer(T_180), convertToPointer(T_270)}},
+    {4, {convertToPointer(S_0), convertToPointer(S_90)}},
+    {5, {convertToPointer(Z_0), convertToPointer(Z_90)}},
+    {6, {convertToPointer(J_0), convertToPointer(J_90), convertToPointer(J_180), convertToPointer(J_270)}},
+    {7, {convertToPointer(L_0), convertToPointer(L_90), convertToPointer(L_180), convertToPointer(L_270)}}
+};
+
+// Define movement and rotation actions using lambda functions with explicit types
+function<void()> moveLeft = [&]() {
+    if (columnStart > 0) columnStart--;
+};
+
+function<void()> moveRight = [&]() {
+    if (columnStart + tetroCols < boardCols) columnStart++;
+};
+
+function<void()> moveDown = [&]() {
+    if (canMoveDown(board, tetromino, tetroRows, tetroCols, currentRow, columnStart)) currentRow++;
+};
+
+function<void()> rotateRight = [&]() {
+    if (rotations[colorPair].size() > 1) {  // Rotate only if multiple rotations are defined
+        rotationIndex = (rotationIndex + 1) % rotations[colorPair].size();
+        tetromino = rotations[colorPair][rotationIndex];
+
+        // Set tetroRows and tetroCols based on the rotation and piece type
+        switch (colorPair) {
+            case 1: // 'I' piece
+                tetroRows = (rotationIndex % 2 == 0) ? 1 : 4;
+                tetroCols = (rotationIndex % 2 == 0) ? 4 : 1;
+                break;
+            case 2: // 'O' piece (no rotation)
+                tetroRows = 2;
+                tetroCols = 2;
+                break;
+            case 3: // 'T' piece
+                tetroRows = (rotationIndex % 2 == 0) ? 2 : 3;
+                tetroCols = (rotationIndex % 2 == 0) ? 3 : 2;
+                break;
+            case 4: // 'S' piece
+            case 5: // 'Z' piece
+                tetroRows = (rotationIndex % 2 == 0) ? 2 : 3;
+                tetroCols = (rotationIndex % 2 == 0) ? 3 : 2;
+                break;
+            case 6: // 'J' piece
+            case 7: // 'L' piece
+                tetroRows = (rotationIndex % 2 == 0) ? 2 : 3;
+                tetroCols = (rotationIndex % 2 == 0) ? 3 : 2;
+                break;
+        }
+    }
+};
+
+function<void()> rotateLeft = [&]() {
+    if (rotations[colorPair].size() > 1) {  // Rotate only if multiple rotations are defined
+        rotationIndex = (rotationIndex - 1 + rotations[colorPair].size()) % rotations[colorPair].size();
+        tetromino = rotations[colorPair][rotationIndex];
+
+        // Set tetroRows and tetroCols based on the rotation and piece type
+        switch (colorPair) {
+            case 1: // 'I' piece
+                tetroRows = (rotationIndex % 2 == 0) ? 1 : 4;
+                tetroCols = (rotationIndex % 2 == 0) ? 4 : 1;
+                break;
+            case 2: // 'O' piece (no rotation)
+                tetroRows = 2;
+                tetroCols = 2;
+                break;
+            case 3: // 'T' piece
+                tetroRows = (rotationIndex % 2 == 0) ? 2 : 3;
+                tetroCols = (rotationIndex % 2 == 0) ? 3 : 2;
+                break;
+            case 4: // 'S' piece
+            case 5: // 'Z' piece
+                tetroRows = (rotationIndex % 2 == 0) ? 2 : 3;
+                tetroCols = (rotationIndex % 2 == 0) ? 3 : 2;
+                break;
+            case 6: // 'J' piece
+            case 7: // 'L' piece
+                tetroRows = (rotationIndex % 2 == 0) ? 2 : 3;
+                tetroCols = (rotationIndex % 2 == 0) ? 3 : 2;
+                break;
+        }
+    }
+};
+
+
+
+// Map of key inputs to corresponding movement functions
+map<int, function<void()>> keyActions = {
+    {'a', moveLeft},
+    {'d', moveRight},
+    {'s', moveDown},
+    {'A', rotateLeft}, // Shift + a for left rotation
+    {'D', rotateRight} // Shift + d for right rotation
+};
