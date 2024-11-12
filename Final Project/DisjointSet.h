4a6c8bbbde0f19ce0f7be24fbedb97cd29252749
NighthawkLabs <116287980+NighthawkLabs@users.noreply.github.com>
2024-11-12 17:14:36 -0500

    Add files via upload

---
+++ Final Project/DisjointSet.h
@@ -0,0 +1,37 @@
+//DisjointSet.h
+#ifndef DISJOINTSET_H
+#define DISJOINTSET_H
+
+#include <vector>
+
+using namespace std;
+
+struct DisjointSet {
+    vector<int> parent;
+    vector<int> size;
+
+    DisjointSet(int n) : parent(n), size(n, 1) {
+        for (int i = 0; i < n; ++i) parent[i] = i;
+    }
+
+    int find(int x) {
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    void unite(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (size[rootX] < size[rootY]) swap(rootX, rootY);
+            parent[rootY] = rootX;
+            size[rootX] += size[rootY];
+        }
+    }
+
+    int getSize(int x) {
+        return size[find(x)];
+    }
+};
+
+#endif //DISJOINTSET_H
